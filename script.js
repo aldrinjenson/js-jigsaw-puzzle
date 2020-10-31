689e3cce1d4aa7647879e1770a8e0116977310be
Aldrin Jenson <aldrinjenson@gmail.com>
2020-10-31 21:38:51 +0530

    Add new images

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ const characterName = document.getElementById("character-name");
 const characterClass = document.getElementById("character-class");
 
 const width = 10;
-const bgPaths = ["1.png", "2.jpg", "3.jpeg", "4.jpeg"];
+const bgPaths = ["0.jpeg", "1.png", "2.jpg", "3.jpeg", "4.jpeg", "5.jpeg"];
 const people = {
-  0: { name: "Jisna Jose", class: "CS7A" },
-  1: { name: "Shimil Abraham", class: "CS3A" },
-  2: { name: "Alan Chettan", class: "LKG-B" },
-  3: { name: "Jency Chechy", class: "EB5" },
+  0: { name: "Aldrin Jenson", class: "CS3B" }, // trial
+  1: { name: "Jisna Jose", class: "CS7A" },
+  2: { name: "Shimil Abraham", class: "CS3A" },
+  3: { name: "Alan Chettan", class: "LKG-B" },
+  4: { name: "Jency Chechy", class: "EB5" },
+  5: { name: "Neema John", class: "ECE" },
 };
 let goldValue = 100;
 let triesValue = 0;
 let index = 0;
 
 gridWrapper.style.backgroundImage = `url(images/${bgPaths[index]})`;
-gameRoundValue.innerText = index + 1;
+gameRoundValue.innerText = index;
 gold.innerText = goldValue;
 tries.innerText = triesValue;
 
@@ -57,7 +59,7 @@ const nextRound = () => {
   characterClass.innerText = "";
   index += 1;
   gridWrapper.style.backgroundImage = `url(images/${bgPaths[index]})`;
-  gameRoundValue.innerText = index + 1;
+  gameRoundValue.innerText = index;
   createBoard();
   if (index + 1 === bgPaths.length) {
     const nextButton = document.getElementById("nextButton");
