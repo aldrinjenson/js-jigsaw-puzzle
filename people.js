90f5c5ccd9df0f1a468199b592ca2beec4ce7e39
Restyled.io <commits@restyled.io>
2020-11-15 07:35:04 +0000

    Restyled by prettier

--- people.js
+++ people.js
@@ -5,10 +5,10 @@
 // in the images folder"
 
 const people = {
-  0 : {name : "St. Thomas Aquinas", alt : "", imgSrc : "0.jpeg"}, // trial
-  1 : {name : "St. Teresa of Avila", alt : "", imgSrc : "1.jpeg"},
-  2 : {name : "Blessed Carlo Acutis", alt : "", imgSrc : "2.jpeg"},
-  3 : {name : "Saint Therese of Lisieux", alt : "", imgSrc : "3.jpeg"},
-  4 : {name : "Saint. Clare", alt : "", imgSrc : "4.jpeg"},
-  5 : {name : "Saint Padre Pio", alt : "", imgSrc : "5.jpeg"},
+  0: { name: "St. Thomas Aquinas", alt: "", imgSrc: "0.jpeg" }, // trial
+  1: { name: "St. Teresa of Avila", alt: "", imgSrc: "1.jpeg" },
+  2: { name: "Blessed Carlo Acutis", alt: "", imgSrc: "2.jpeg" },
+  3: { name: "Saint Therese of Lisieux", alt: "", imgSrc: "3.jpeg" },
+  4: { name: "Saint. Clare", alt: "", imgSrc: "4.jpeg" },
+  5: { name: "Saint Padre Pio", alt: "", imgSrc: "5.jpeg" },
 };
