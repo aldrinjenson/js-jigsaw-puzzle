69ce0236f237d559f72ba229e16c77af46895e10
Restyled.io <commits@restyled.io>
2020-11-15 07:35:02 +0000

    Restyled by clang-format

--- people.js
+++ people.js
@@ -1,13 +1,14 @@
 // format:
 // name: "Name of the person"
-// alt: "Class of the person or any other alternative text. Make it empty for displaying nothing"
-// imgSrc: "enter full name of the image file as specified in the images folder"
+// alt: "Class of the person or any other alternative text. Make it empty for
+// displaying nothing" imgSrc: "enter full name of the image file as specified
+// in the images folder"
 
 const people = {
-  0: { name: "St. Thomas Aquinas", alt: "", imgSrc: "0.jpeg" }, // trial
-  1: { name: "St. Teresa of Avila", alt: "", imgSrc: "1.jpeg" },
-  2: { name: "Blessed Carlo Acutis", alt: "", imgSrc: "2.jpeg" },
-  3: { name: "Saint Therese of Lisieux", alt: "", imgSrc: "3.jpeg" },
-  4: { name: "Saint. Clare", alt: "", imgSrc: "4.jpeg" },
-  5: { name: "Saint Padre Pio", alt: "", imgSrc: "5.jpeg" },
+  0 : {name : "St. Thomas Aquinas", alt : "", imgSrc : "0.jpeg"}, // trial
+  1 : {name : "St. Teresa of Avila", alt : "", imgSrc : "1.jpeg"},
+  2 : {name : "Blessed Carlo Acutis", alt : "", imgSrc : "2.jpeg"},
+  3 : {name : "Saint Therese of Lisieux", alt : "", imgSrc : "3.jpeg"},
+  4 : {name : "Saint. Clare", alt : "", imgSrc : "4.jpeg"},
+  5 : {name : "Saint Padre Pio", alt : "", imgSrc : "5.jpeg"},
 };
